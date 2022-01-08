79d73848ad95bdb7ac90beeda1780ec7289809c3
web77 <web77@yandex.ru>
2022-01-08 18:54:53 +0300

    AppHeader - first iteration done

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './AppHeader.module.css';
 
 class AppHeader extends React.Component {
@@ -8,16 +8,30 @@ class AppHeader extends React.Component {
       <header className={styles.header}>
         <nav className={styles.navigation}>
           <ul className={styles.list}>
-            <li><a href="#" className="text text_type_main-default">Конструктор</a></li>
-            <li><a href="#" className={styles.link}>Лента заказов</a></li>
+            <li>
+              <div className={styles.button + " pt-4 pr-5 pb-4 pl-5"}>
+                <BurgerIcon type="primary" />
+                <p className={styles.link + " text text_type_main-default ml-2"}>Конструктор</p>
+              </div>
+            </li>
+            <li className="ml-2">
+              <div className={styles.button + " pt-4 pr-5 pb-4 pl-5"}>
+                <ListIcon type="secondary" />
+                <p className="text text_type_main-default text_color_inactive ml-2">Лента заказов</p>
+              </div>
+            </li>
           </ul>
         </nav>
         <Logo />
-        <a href="#" className={styles.link}>Личный кабинет</a>
+        <div className={styles.rightSide}>
+          <div className={styles.button + " pt-4 pr-5 pb-4 pl-5"}>
+            <ProfileIcon type="secondary" />
+            <p className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</p>
+          </div>
+        </div>
       </header>
     );
   }
-  
 }
 
 export default AppHeader;
