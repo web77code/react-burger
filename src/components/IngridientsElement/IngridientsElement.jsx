7be84c06c8d0d922cc1c8b4fca6395b265e778fb
web77 <web77@yandex.ru>
2022-01-14 09:05:20 +0300

    BurgerIngridients layout ready

--- src/components/IngridientsElement/IngridientsElement.jsx
+++ src/components/IngridientsElement/IngridientsElement.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import ingridientStyles from './IngridientsElement.module.css';
 
 class IngridientsElement extends React.Component {
   render () {
     return (
       <div className={ingridientStyles.ingridient}>
-        <img src={this.props.image} className={ingridientStyles.image} alt={this.props.name} />
-        <div className={ingridientStyles.price}>{this.props.price}</div>
-        <h3 className={ingridientStyles.name}>{this.props.name}</h3>
+        <img src={this.props.image} className={"mb-1 " + ingridientStyles.image} alt={this.props.name} />
+        <div className={"mb-1 " + ingridientStyles.priceContainer}>
+          <p className={"text text_type_digits-default " + ingridientStyles.price}>{this.props.price}</p>
+          <CurrencyIcon type="primary" />
+          </div>
+        <h3 className={"text text_type_main-default " + ingridientStyles.name}>{this.props.name}</h3>
       </div>
     );
   }
