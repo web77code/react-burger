c73e7f4dd758703ec0916105e9fcf3e715973043
web77code <web77@yandex.ru>
2022-07-04 09:39:00 +0300

    refact: make main index.jsx more clearly

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import store from './services/store';
-import App from './components/app/app';
-import './index.css';
+
+import { App } from './components/app';
 
 ReactDOM.render(
   <React.StrictMode>
