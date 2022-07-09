52b0080a54fbe87e0c89959044fc4491dfc5f12a
web77code <web77@yandex.ru>
2022-07-09 21:30:54 +0300

    feat: add state for authonly redirect

--- src/components/protected-route/protected-route.jsx
+++ src/components/protected-route/protected-route.jsx
@@ -1,35 +1,41 @@
-import { useSelector } from 'react-redux';
-import { Redirect, Route, useHistory } from 'react-router-dom';
+import { useRef } from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route, useHistory } from "react-router-dom";
 
-const ProtectedRoute = ({ 
-  anonymousOnly = false, 
+const ProtectedRoute = ({
+  anonymousOnly = false,
   authOnly = false,
   hasParrentPage = false,
-  ...rest }
-  ) => {
-
+  ...rest
+}) => {
   const history = useHistory();
+  const ref = useRef();
+
   const { isAuthChecked, data } = useSelector((store) => store.user);
 
-  if(hasParrentPage) {
-    if(history.location.state) {
-      if(hasParrentPage !== history.location.state.from) {
-        return <Redirect to={hasParrentPage} />
+  if (hasParrentPage) {
+    if (history.location.state) {
+      if (hasParrentPage !== history.location.state.from) {
+        return <Redirect to={hasParrentPage} />;
       }
     } else {
-      return <Redirect to={hasParrentPage} />
+      return <Redirect to={hasParrentPage} />;
     }
   }
 
-  if(anonymousOnly && data) {
-    return <Redirect to="/" />
+  if (anonymousOnly && data !== null) {
+    if(ref.current) {
+      return <Redirect to={ref.current} />;
+    } 
+    return <Redirect to="/" />; 
   }
 
-  if(authOnly && (isAuthChecked && !data)) {
-    return <Redirect to="/login" />
+  if (authOnly && isAuthChecked && data === null) {
+    ref.current = history.location.pathname;
+    return <Redirect to="/login" />;
   }
 
   return <Route {...rest} />;
-}
+};
 
 export default ProtectedRoute;
