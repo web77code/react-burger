a43baf4b9d5fbedf35a9717e5617136ed2fbc3c4
web77code <web77@yandex.ru>
2022-07-10 14:40:27 +0300

    feat: add proptypes check for dnd-element

--- src/components/dnd-element/dnd-element.jsx
+++ src/components/dnd-element/dnd-element.jsx
@@ -1,10 +1,13 @@
+import PropTypes from 'prop-types';
 import { useDrag, useDrop } from 'react-dnd';
 
 import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './dnd-element.module.css';
 
-const DndElement = ({ name, price, image, uid, findCard, moveCard, onDelete }) => {
+const DndElement = (props) => {
+
+  const { name, price, image, uid, findCard, moveCard, onDelete } = props;
   
   const originalIndex = findCard(uid).index;
 
@@ -57,6 +60,16 @@ const DndElement = ({ name, price, image, uid, findCard, moveCard, onDelete }) =
       />
     </div>
   );
+};
+
+DndElement.propTypes = {
+  name: PropTypes.string.isRequired, 
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  uid: PropTypes.string.isRequired,
+  findCard: PropTypes.func.isRequired,
+  moveCard: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default DndElement;
