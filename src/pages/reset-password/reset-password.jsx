c3760e0e159b402925824518e714d8eb9f1cd039
web77code <web77@yandex.ru>
2022-07-10 14:12:20 +0300

    feat: add animated loader to ResetPassword page

--- src/pages/reset-password/reset-password.jsx
+++ src/pages/reset-password/reset-password.jsx
@@ -5,6 +5,8 @@ import { Input, PasswordInput, Button } from '@ya.praktikum/react-developer-burg
 import { updateUserPassword } from '../../utils/api';
 import { logErrorToConsole } from '../../utils/utils';
 
+import AnimatedLoader from "../../components/animated-loader";
+
 import styles from './reset-password.module.css';;
 
 const ResetPassword = () => {
@@ -14,6 +16,7 @@ const ResetPassword = () => {
     token: '',
   });
   const [response, setResponse] = useState(false);
+  const [process, setProcess] = useState(false);
 
   const onChange = (e) => {
     const { name, value } = e.target;
@@ -23,59 +26,66 @@ const ResetPassword = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    setProcess(true);
 
     updateUserPassword(state)
       .then((res) => {
-        if(res.success) setResponse(true);
+        if (res.success) {
+          setProcess(false);
+          setResponse(true);
+        }
       })
       .catch((err) => {
         logErrorToConsole(err);
-      });
+      })
+      .finally(() => {
+        setProcess(false);
+      })
   }
 
   return (
     <>
-      {
-        response ? (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: { from: '/reset-password' }
-            }}
-          />
-          ) : (
-          <div className={styles.container}>
-            <h1 className="mb-6 text text_type_main-medium">Восстановление пароля</h1>
-            <form className={'mb-20 ' + styles.form} onSubmit={onSubmit}>
-              <div className={'mb-6 ' + styles.input}>
-                <PasswordInput 
-                  onChange={onChange} 
-                  value={state.password} 
-                  name={'password'} 
-                />
-              </div>
-              <div className={'mb-6 ' + styles.input}>
-                <Input
-                  type={'text'}
-                  placeholder={'Введите код из письма'}
-                  onChange={onChange} 
-                  value={state.token} 
-                  error={false}
-                  errorText={"Ошибка"}
-                  name={'token'}
-                  size={"default"}
-                />
-              </div>        
-              <Button type="primary" size="medium">
-                Сохранить
-              </Button>
-            </form>
-            <p className="mb-4 text text_type_main-default text_color_inactive">
-              Вспомнили пароль? <Link to="/login" className={styles.link}>Войти</Link>
-            </p>
-          </div>
-        )
-      }
+      {process && <AnimatedLoader />}
+
+      {response ? (
+        <Redirect
+          to={{
+            pathname: '/login',
+            state: { from: '/reset-password' }
+          }}
+        />
+        ) : (
+        <div className={styles.container}>
+          <h1 className="mb-6 text text_type_main-medium">Восстановление пароля</h1>
+          <form className={'mb-20 ' + styles.form} onSubmit={onSubmit}>
+            <div className={'mb-6 ' + styles.input}>
+              <PasswordInput 
+                onChange={onChange} 
+                value={state.password} 
+                name={'password'} 
+              />
+            </div>
+            <div className={'mb-6 ' + styles.input}>
+              <Input
+                type={'text'}
+                placeholder={'Введите код из письма'}
+                onChange={onChange} 
+                value={state.token} 
+                error={false}
+                errorText={"Ошибка"}
+                name={'token'}
+                size={"default"}
+              />
+            </div>        
+            <Button type="primary" size="medium">
+              Сохранить
+            </Button>
+          </form>
+          <p className="mb-4 text text_type_main-default text_color_inactive">
+            Вспомнили пароль? <Link to="/login" className={styles.link}>Войти</Link>
+          </p>
+        </div>
+      )}
     </>
     
   );
