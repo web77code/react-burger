67d106ae55d5cd15a4a43cdb5e741c7a5b577e97
web77code <web77@yandex.ru>
2022-07-20 11:06:43 +0300

    feat: add basic logic for get orders history

--- src/pages/profile-orders/profile-orders.jsx
+++ src/pages/profile-orders/profile-orders.jsx
@@ -1,12 +1,33 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { getCookie } from "../../utils/cookies";
+
+import { WS_URL } from "../../utils/constants";
+import { WS_CONNECTION_START } from "../../services/actions/orders";
+
 import ProfileSidebar from "../../components/profile-sidebar";
+import FeedList from "../../components/feed-list";
 
 import styles from "./profile-orders.module.css";
 
 const ProfileOrders = () => {
+  const dispatch = useDispatch();
+
+  const orders = useSelector((store) => store.feed.orders);
+
+  useEffect(() => {
+    const accessToken = getCookie("token");
+    const wsUrl = `${WS_URL.personalFeed}?token=${accessToken}`;
+
+    dispatch({ type: WS_CONNECTION_START, payload: wsUrl });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className={"mt-30 " + styles.container}>
       <ProfileSidebar description="В этом разделе вы можете посмотреть свою историю заказов" />
-      <h2 className="text text_type_main-medium">История заказов</h2>
+      {orders.length > 0 && <FeedList />}
     </div>
   );
 };
